9fdd10fb65869515ceeec880ce3e7b73036be795
Chris Michels <chrisjohnmichels@gmail.com>
2018-06-08 22:01:25 -0400

    read database info

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+
+
+var user = models.user.findOne().then(function(user){
+    console.log(user.username)
+    return user.username;
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    models.user.findById(1)
+    .then( user =>{
+        models.message.findById(1)
+            .then(message => {
+                models.tag.findById(1)
+                    .then(tag => {
+                            res.render ('index', {
+                            title: 'tagged',
+                            user: user.username,
+                            message: message.message,
+                            tag: tag.tagnum
+                    })
+            })
+        })
+    })
 });
 
 module.exports = router;
