fa92e8d00aa4a03ea2929d9ea7507927316dcc5c
Chris Michels <chrisjohnmichels@gmail.com>
2018-06-10 20:25:27 -0400

    added tag search api

--- routes/index.js
+++ routes/index.js
@@ -9,26 +9,48 @@ var user = models.user.findOne().then(function(user){
 })
 
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     models.user.findById(1,{
-        include: [
-            models.message
-        ]
-    })
-    .then( user =>{
-        models.tag.findById(1)
-        .then(tag => {
-            res.render('index', {
-                title: 'tagged',
-                user: user.username,
-                //use handlebars to show all messages related to user
-                message: user.messages[0].message,
-                tag: tag.tagnum
+        include: [models.message]
+                })
+                .then( user =>{
+                    models.tag.findById(1)
+                    .then(tag => {
+                     res.render('index',
+                     {
+                            title: 'tagged',
+                            user: user.username,
+                            //use handlebars to show all messages related to user
+                            message: user.messages[0].message,
+                            tag: tag.tagnum
             })
         })
 
     })
 });
+
+
+
+router.get('/:state/:id', (req, res) => {
+    const id = req.params.id;
+    const state = req.params.state;
+    models.tag.findAll({
+        where: {tagnum: id, state: state},
+        include: [{
+            model: models.message,
+            attributes:['message'],
+        }]
+    }).then(tag => {
+        res.send(JSON.stringify(tag[0].messages))
+    })
+});
+
+
+router.post('/:id', (req, res) => {
+
+})
+
 
 module.exports = router;
