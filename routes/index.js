d6187f674e0b5bd863f358c8224b81f12050e516
Chris Michels <chrisjohnmichels@gmail.com>
2018-06-12 21:11:59 -0400

    changed test response

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 const ensureAuthenticated = require('../auth').ensureAuthenticated;
+const bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,20 +28,21 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/test', (req,res) =>{ 
-     models.user.findAll({
+    if(req.isAuthenticated()){
+     models.user.findById(req.user,{
         include: [{
-        model: models.tag,
-         through: {
-      attributes: ['tagId', 'userId'],
-      where: {userId: req.user}
-         }
-     }]
- })
+            model: models.tag,
+            include: [{model: models.message, attributes:['message']}],
+            attributes:['tagnum']
+         }]
+     })
     .then(user => {
         res.render('error', {
-            message: JSON.stringify(user[0].tags)
+            message: JSON.stringify(user.tags, null, "\t")
     })
-})
+})}else{
+        res.render('index')
+    }
 });
 
 
@@ -59,6 +61,19 @@ router.get('/:state/:id', (req, res) => {
     })
 });
 
+
+router.post('/', (req, res)=>{
+    // if(req.isAuthenticated()){
+    console.log(req.body);
+        models.message.create({
+        userId: req.body.userId,
+        tagId: req.body.tagId,
+        message: req.body.message
+     })
+    // }else{
+    //     res.render('index')
+    // }
+})
 
 
 module.exports = router;
